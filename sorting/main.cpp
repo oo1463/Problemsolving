858e2a6a43bb2f501dff7adf1cf16506cb566752
oo1463 <oooo1463@gmail.com>
2020-12-27 19:57:34 +0900

    ADD : sort - selection sort

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 #include "bubblesort.hpp"
+#include "selectionsort.hpp"
 #include <vector>
 using namespace std;
 
 int arr[10] = {1, 6, 3, 7, 2, 4, 5, 0, 9, 8};
 vector<int> arr2;
 
+void print_arr(int *arr){
+    for(int i=0; i<10; i++) cout << arr[i] << ' ';
+    cout << endl;
+}
+
 int main(){
-    bubble(arr);
-    for(int i=0; i<10; i++){
-        arr2.push_back(arr[i]);
-    }
 
-    bubble(arr2);
+    cout << "Before sort array : ";
+    print_arr(arr);
+    // bubble(arr);
+    // for(int i=0; i<10; i++){
+    //     arr2.push_back(arr[i]);
+    // }
 
-    for(auto a : arr2) {
+    // bubble(arr2);
+
+    selection(arr);
+
+    cout << "After sort array : ";
+    for(auto a : arr) {
         cout << a << ' ';
     }
 }
--- sorting/selectionsort.hpp
+++ sorting/selectionsort.hpp
@@ -2,5 +2,15 @@
 #include <vector>
 
 void selection(int *arr){  // array sort
-    
+    for(int i=0; i<9; i++){
+        int mn = arr[i];
+        int min_index = i;
+        for(int j=i; j<10; j++){
+            if(mn > arr[j]) {
+                mn = arr[j];
+                min_index = j; 
+            }
+        }
+        std::swap(arr[i], arr[min_index]);
+    }
 }
