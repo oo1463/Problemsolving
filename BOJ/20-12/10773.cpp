7dbb567f3eee3d68d3309500ce14a41e15d6cd5f
Yujun Jang <oooo1463@gmail.com>
2020-12-28 13:49:35 +0900

    ADD : boj 10773

---
+++ BOJ/20-12/10773.cpp
@@ -0,0 +1,47 @@
+// https://www.acmicpc.net/problem/10773
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <list>
+#define el '\n'
+#define FAIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+
+using namespace std;
+
+int K;
+
+int main() {
+    FAIO;
+
+    cin >> K;    
+
+    list<int> l;
+
+    int sum = 0;
+    while(K--){
+        int ti;
+        cin >> ti;
+        if(ti != 0) l.push_back(ti);
+        else{
+            l.pop_back();
+        }
+    }
+    
+    for(auto a : l) sum += a;
+
+    cout << sum;
+
+    return 0;
+}
+
+
+
+/*
+    Algorithm : 
+
+    Time complexity : O(N^2)
+    
+*/
