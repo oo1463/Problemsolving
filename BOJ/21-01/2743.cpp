5766ef0619446ad2a58d5eb33c9edc400425f22a
Yujun Jang <oooo1463@gmail.com>
2021-01-22 22:27:17 +0900

    ADD : boj 2743

---
+++ BOJ/21-01/2743.cpp
@@ -0,0 +1,34 @@
+// https://www.acmicpc.net/problem/2743
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#define el '\n'
+#define FAIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+
+using namespace std;
+
+
+int main() {
+    FAIO;
+
+    string s;
+
+    cin >> s;
+
+    cout << s.size();
+
+   
+    return 0;
+}
+
+
+
+/*
+    Algorithm : 
+
+    Time complexity : O(N^2)
+    
+*/
