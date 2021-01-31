e1a8b7df787982ccdab660d3a63500425530ba22
oo1463 <oooo1463@gmail.com>
2021-01-31 20:15:18 +0900

    ADD BOJ 5573

---
+++ BOJ/21-01/5573.cpp
@@ -0,0 +1,86 @@
+// https://www.acmicpc.net/problem/5573
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#define el '\n'
+#define FAIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+
+using namespace std;
+
+int W,H,N;
+bool map[1001][1001];
+bool n_map[1001][1001];
+int dp[1001][1001];
+
+int main() {
+    FAIO;
+
+    cin >> H >> W >> N;
+
+    for(int i=1; i<=H; i++){
+        for(int j=1; j<=W; j++){
+            cin >> map[i][j];
+        }
+    }
+
+    for(int i=1; i<=H; i++){
+        for(int j=1; j<=W; j++){
+            if(i==1 && j == 1) dp[i][j] = N-1;
+            else {
+                int tmp1, tmp2;
+                if(map[i][j-1] && dp[i][j-1] % 2 == 1){
+                    tmp1 = dp[i][j-1]/2 + 1;
+                }
+                else{
+                    tmp1 = dp[i][j-1]/2;
+                }
+                if(!map[i-1][j] && dp[i-1][j] % 2 == 1){
+                    tmp2 = dp[i-1][j]/2 + 1;
+                }
+                else{
+                    tmp2 = dp[i-1][j]/2;
+                }
+
+                dp[i][j] = tmp1 + tmp2;
+            }
+
+            if(dp[i][j] % 2 == 1){
+                n_map[i][j] = !map[i][j];
+            }
+            else{
+                n_map[i][j] = map[i][j];
+            }
+
+        }
+    }
+
+    int x=1 ,y = 1;
+
+    while(x <= H && y <= W){
+        if(n_map[x][y] == false){
+            n_map[x][y] = true;
+            x++;
+        }
+        else if(n_map[x][y] == true){
+            n_map[x][y] = false;
+            y++;
+        }
+    }
+
+    cout << x << ' ' << y;
+
+
+   
+    return 0;
+}
+
+
+
+/*
+    Algorithm : DP
+
+    Time complexity : O(W*H)
+    
+*/
