8c1aeb69a8f66abc3a06a77679aa61c33999fc96
oo1463 <oooo1463@gmail.com>
2021-02-11 21:50:16 +0900

    ADD boj 2042

--- BOJ/21-02/2042.cpp
+++ BOJ/21-02/2042.cpp
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/2042
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,20 +13,20 @@ using namespace std;
 // 세그먼트 트리의 크기는 배열의 크기보다 큰 가장 가까운 N의 제곱수를 구하고 그것의 2배이다.
 // ex. N == 100000 일 때 
 // 트리의 크기 : 102001*2 = 204002
-int tree[204002];
-int arr[100000];
+long long tree[3*1000001];
+long long arr[1000001];
 
 // 세그먼트트리 초기화
-int init(int start, int end, int node){
+long long init(long long start, long long end, long long node){
     if(start == end) return tree[node] = arr[start]; // 만약 원소가 하나인 경우 tree[node]는 해당 배열의 원소
 
-    int mid = (start + end) / 2;
+    long long mid = (start + end) / 2;
 
     // 해당 노드는 왼쪽 자식의 노드 + 오른쪽 자식의 노드
     return tree[node] = init(start,mid, node*2) + init(mid+1 , end, node*2 + 1);
 }
 
-int sum(int start, int end, int node, int left, int right){ 
+long long sum(long long start, long long end, long long node, long long left, long long right){ 
     // 범위 밖에 있는 경우
     if(left > end || right < start) return 0;
 
@@ -35,30 +37,48 @@ int sum(int start, int end, int node, int left, int right){
 }
 
 // index : 수정하고자 하는 노드의 index, diff : 해당 값에 더하고자 하는 값
-void update(int start, int end, int node, int index, int diff){
+void update(long long start, long long end, long long node, long long index, long long diff){
     if(index < start || index > end) return;
 
     tree[node] += diff;
-    if(start == end) return;
-    int mid = (start + end) / 2;
+    if(start == end) {
+        arr[start] += diff;
+        return;
+    }
+    long long mid = (start + end) / 2;
 
     update(start, mid, node*2, index, diff);
-    update(mid, end, node*2+1, index, diff);
+    update(mid+1, end, node*2+1, index, diff);
     
 }
+
+long long N, M, K;
 
 int main() {
     FAIO;
     
+    cin >> N >> M >> K;
+    for(long long i=1; i<=N; i++) cin >> arr[i];
+    init(1, N, 1);
+    for(long long i=0; i<M + K; i++) {
+        long long what, where, how;
+        cin >> what >> where >> how;
+        if(what == 1){
+            update(1, N, 1, where, how-arr[where]);
+        }
+        else if(what == 2){
+            cout << sum(1, N, 1, where, how) << el;
+        }
+
+    }
    
     return 0;
 }
 
 
-
 /*
-    Algorithm : 
+    Algorithm : segment tree
 
-    Time complexity : O(N^2)
+    Time complexity : O(NlogN)
     
 */
